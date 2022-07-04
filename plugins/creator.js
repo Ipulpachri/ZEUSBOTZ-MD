65f6c6a91e2cfbc0f71679963d57a0f6bb4e7806
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-04 10:14:20 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,14 +1,23 @@
 function handler(m) {
 const PhoneNumber = require('awesome-phonenumber')
-let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'Centauri', sellerJid: '0@s.whatsapp.net'}}}                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'Centauri', sellerJid: '0@s.whatsapp.net'}}}
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:; Pilar;;;'
-                    + 'FN: PilarXcode\n' // full name
-                    + 'ORG:Pilar;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6289625556161:+628-962-555-6161\n' // WhatsApp ID + phone number
+                    + 'N:; Fachri;;;'
+                    + 'FN: FachriOwnBot\n' // full name
+                    + 'ORG:Tak ingin terkenal hanya ingin di kenal;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6285713041886:+62857-1304-1886\n'
+                    + 'X-ABLabel:Sibuk\n' 
+                    + 'EMAIL;type=INTERNET:saefulfachri18@gmail.com\n'
+                    + 'X-ABLabel:Email\n'
+                    + 'ADR:;;🇮🇩 Indonesia;;;;\n'
+                    + 'X-ABADR:ac\n'
+                    + 'X-ABLabel:Region\n'
+                    + 'URL:https://fachriweb.vercel.app\n'
+                    + 'X-ABLabel:Website'
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'PilarXcode', contacts: [{ vcard }] } }, { quoted: ftroli })
-m.reply(m.chat,'*Tuh nomor owner ku*\n*Chat jika PENTING*', m)
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Fachri', contacts: [{ vcard }] } }, { quoted: ftroli })
+m.reply(m.chat,'*Tuh nomor org ganteng*', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
